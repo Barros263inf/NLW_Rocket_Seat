2c0091017240f11212e2ecd7d61341e79d720cd0
Daniel Da Silva Barros <94022875+Barros263inf@users.noreply.github.com>
2022-05-06 23:31:04 -0300

    Update Aula 2

--- script/menu_on.js
+++ script/menu_on.js
@@ -1,9 +1,24 @@
+
 function menu_on(){
-    const menu = document.getElementById("menu_barra");
-    if(scrollY >= 50){
-        menu.classList.add("scroll");
+    if(scrollY > 0){
+        menu_barra.classList.add("scroll");
     }
     else{
-        menu.classList.remove("scroll");
+        menu_barra.classList.remove("scroll");
     }
+}
+
+function menu_show(){
+    const menu = document.getElementById("menu_exp");
+    menu.classList.toggle("menu_on")
+    /*if(menu.classList.contains("menu_off")){
+        alert(true)
+        menu.classList.remove("menu_off");
+        menu.classList.add("menu_on");
+    }
+    else{
+        alert(false)
+        menu.classList.remove("menu_on");
+        menu.classList.add("menu_off");
+    }*/
 }
