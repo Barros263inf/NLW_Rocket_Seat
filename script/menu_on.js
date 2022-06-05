a3604e4732a32947aa8ea00fdb14a8849a92afb6
Daniel Da Silva Barros <94022875+Barros263inf@users.noreply.github.com>
2022-06-05 00:00:28 -0300

    final update

--- script/menu_on.js
+++ script/menu_on.js
@@ -1,12 +1,13 @@
-
 window.addEventListener("scroll", on_scroll)
 
-on_scroll()
+on_scroll();
 
-function on_scroll() {
-    menubar_active_on_scroll()
+function on_scroll(){
+    menubar_active_on_scroll();
     show_arrow_button_on_scroll();
-    active_menu_on_scroll_at_current_section(home)
+    active_menu_at_current_section_on_scroll(home);
+    active_menu_at_current_section_on_scroll(services);
+    active_menu_at_current_section_on_scroll(about);
 }
 
 function menubar_active_on_scroll() {
@@ -26,27 +27,28 @@ function show_arrow_button_on_scroll() {
     }
 }
 
-function active_menu_on_scroll_at_current_section(section) {
+function active_menu_at_current_section_on_scroll(current_section) {
+
     const target_line = scrollY + innerHeight / 2;
+    
+    const section_id = current_section.getAttribute("id")
+    const section_top = current_section.offsetTop;
+    const section_height = current_section.offsetHeight;
+    const section_end = section_top + section_height;
 
-    let section_off = section.offsetTop;
-    let section_height = section.offsetHeight;
-    let section_ends_at = section_off + section_height;
+    const menu_element = document.querySelector(`.links li a[href*=${section_id}]`);
 
-
-    if (section_off <= target_line && target_line <= section_ends_at) {
-        console.log("dentro");
-        console.log(target_line);
-        console.log(section_off);
-        console.log(section_ends_at);
+    
+    if(section_top <= target_line && section_end >= target_line){
+        menu_element.classList.add("on");
     }
-    else {
-        console.log("fora");
-        console.log(target_line);
-        console.log(section_off);
-        console.log(section_ends_at);
+    else{
+        menu_element.classList.remove("on");
+
     }
 }
+
+
 
 function menu(value) {
     if (value == "on") {
@@ -57,25 +59,30 @@ function menu(value) {
         menu_exp.classList.remove("menu_on");
         menu_exp.classList.add("menu_off");
     }
-    /*
-    2ª Solution
+    /*2ª Solution
     const menu = document.getElementById("menu_exp");
     menu.classList.toggle("menu_on"
     )*/
-    /*
-    1ª Solution
-    if (menu_exp.classList.contains("menu_off")) {
 
+    /*3ª Solution
+    if (menu_exp.classList.contains("menu_off")) {
         menu_exp.classList.remove("menu_off");
         menu_exp.classList.add("menu_on");
     }
     else {
-
         menu_exp.classList.remove("menu_on");
         menu_exp.classList.add("menu_off");
-    }*/
+    }
+}*/
 }
 
+//Function Button Arrow Up
+arrow_top.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
 
 ScrollReveal({
     origin: "top",
@@ -87,34 +94,3 @@ ScrollReveal({
 #about , #about header, #about p, #about img ,
 #contact ,#contact header,  #contact p, #contact img,
 #footer`);
-
-//Function Menu Bar Scroll
-/*
-window.addEventListener("scroll", () => {
-    if (scrollY > 0) {
-        menu_bar.classList.add("scroll");
-    }
-    else{
-        menu_bar.classList.remove("scroll");
-    }
-})
-*/
-
-//Function Button Arrow Up Opacity
-/*
-window.addEventListener("scroll", () => {
-    if (window.pageYOffset < 1080) {
-        arrow_top.style.display = "none";
-    } else {
-        arrow_top.style.display = "block";
-    }
-})
-*/
-
-//Function Button Arrow Up
-arrow_top.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
-})
